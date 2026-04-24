a18e31519fef7c50e4f054e7f34ab3da5e6ffd32
JulianLipa <lipajulianp@gmail.com>
2026-04-24 18:11:01 -0300

    styles card

--- src/renderer/src/components/CardDataHandler/CardDataHandler.jsx
+++ src/renderer/src/components/CardDataHandler/CardDataHandler.jsx
@@ -17,26 +17,30 @@ const CardDataHandler = ({ data }) => {
   return (
     <div className={`montserrat ${styles.card}`}>
       {/* 🔹 HEADER */}
-      <p className="thin">{data.type?.toUpperCase()} N° {data.id}</p>
+      <div className={`flex w-full blue bg-white ${styles.cardTitle}`}>
+        <p className="thin blue">
+          {data.type?.toUpperCase()} N° {data.id}
+        </p>
+      </div>
 
       {/* 🔹 ALQUILER */}
       {data.type === "alquiler" && (
-        <>
+        <div>
           <p>{data.id}</p>
           <p>
             {data?.locador?.apellido} - {data?.locatario?.apellido}
           </p>
           <p>{data?.inmueble?.direccion}</p>
-        </>
+        </div>
       )}
 
       {/* 🔹 RECIBO */}
       {data.type === "recibo" && (
-        <>
+        <div>
           <p>{data?.nroAlquiler}</p>
           <p>Importe: ${data?.importe}</p>
           <p>{data?.periodo}</p>
-        </>
+        </div>
       )}
 
       {/* 🔹 FALLBACK (por si viene otra cosa) */}
